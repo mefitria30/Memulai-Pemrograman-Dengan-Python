015165060784dc3e4fb4c96e359da3bc57c90f1d
Fitria Melliyanni <fitria.melliyanni@dataon.com>
2025-01-12 22:10:41 +0700

    ekspresi python

---
+++ 13. Pengertian Ekspresi/main.py
@@ -0,0 +1,29 @@
+# Ekspresi pada pemrograman merupakan kombinasi dari satu atau lebih variabel, konstanta, operator, dan fungsi yang bermakna untuk menghasilkan suatu nilai dalam suatu tipe tertentu.
+
+
+x = 10
+y = 2
+result = x - y
+
+print(result)
+
+
+print('------------------------------')
+
+# melakukan penggabungan list.
+
+angka = [2, 4, 6, 8]
+huruf = ['P', 'Y', 'T', 'H', 'O', 'N']
+gabung = angka + huruf
+
+print(gabung)
+
+print('------------------------------')
+
+
+# mereplikasi list dengan menggunakan operator perkalian (*).
+
+learn = ['P', 'Y', 'T', 'H', 'O', 'N']
+replikasi = learn * 2
+
+print(replikasi)
