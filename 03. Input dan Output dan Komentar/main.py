aab52a1b00fd3193e337317616a8ef51fda3dfea
Fitria Melliyanni <fitria.melliyanni@dataon.com>
2025-01-12 11:04:05 +0700

    update coding

--- 03. Input dan Output dan Komentar/main.py
+++ 03. Input dan Output dan Komentar/main.py
@@ -3,3 +3,13 @@
 
 name = input('Masukan nama Anda: ')
  
+# Output
+# Sebenarnya, Anda telah mengenal output dari materi sebelumnya. Fungsi print() yang sebelumnya Anda gunakan adalah perintah untuk menampilkan output ke layar komputer Anda. Mari gabungkan fungsi input() dan output() berdasarkan contoh kode sebelumnya.
+
+print(name)
+
+"""
+Ini adalah Block Comment,
+Teks ini akan diabaikan oleh Python.
+"""
+print("Hello World!")
