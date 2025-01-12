16265bd9c86744b860cb01d24c69de3c48bb2ed0
Fitria Melliyanni <fitria.melliyanni@dataon.com>
2025-01-12 22:48:26 +0700

    update coding

---
+++ 15. Jenis-Jenis Operator/main.py
@@ -0,0 +1,87 @@
+# Operator Aritmetika
+
+x = 11
+y = 5
+
+print(x + y)
+print(x - y)
+print(x * y)
+print(x // y)
+print(x / y)
+print(x % y)
+print(x ** y)
+
+
+print('------------------------------------------------')
+
+# Operator Relasional -> operator perbandingan antara dua operan yang berupa integer, float, string, ataupun boolean.
+
+x = 5
+y = 10
+
+print(x == y)
+print(x != y)
+print(x > y)
+print(x < y)
+print(x >= y)
+print(x <= y)
+
+print('------------------------------------')
+
+x = "fitria"
+y = "melliyanni"
+
+print(x == y)
+print(x != y)
+print(x > y)
+print(x < y)
+print(x >= y)
+print(x <= y)
+
+
+print('-----------------------------')
+
+# Operator logika merupakan jenis operator untuk melakukan operasi logika dengan kedua operannya bertipe boolean.
+
+print(True and True)
+print(True and False)
+print(False and True)
+print(False and False)
+
+print(True or True)
+print(True or False)
+print(False or True)
+print(False or False)
+
+print(not True)
+print(not False)
+
+
+print('-------------------------------')
+
+# Operator Assignment -> bertujuan untuk melakukan proses assignment atau pemberian nilai pada suatu variabel dengan nilai tetap.
+
+# +=
+x = 11
+x += 5
+print(x)
+
+# -=
+x = 11
+x -= 5
+print(x)
+
+# *=
+x = 11
+x *= 5
+print(x)
+
+# /=
+x = 11
+x /= 5
+print(x)
+
+#%=
+x = 11
+x%= 5
+print(x)
---
+++ 17. Soal kategori - Ekspresi/main.py
@@ -0,0 +1,10 @@
+x = 1960 
+x += 901 
+print(x) 
+
+
+print('----------------------')
+
+x = True 
+y = False
+print(x or y)
